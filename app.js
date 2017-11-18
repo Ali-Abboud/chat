2a31d7876b15a24308bbb812bc1b2410b796e32a
thecode007 <finchfisher@gmail.com>
2017-11-18 17:25:53 +0200

    updating checking

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ app.post('/checkingContacts',jsonParser,function (req,res,next) {
 	var data=req.body;
     var i=0;
     var j=0;
-
+    console.log("updated hecking contacts");
     var contacts=[];///for storing matched contacts
     var foundIndex=[];///for storing the index of matched so no doublicated numbers exists
     	Client.find({},function(err,clients){
